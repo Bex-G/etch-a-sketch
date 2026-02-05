e6cfdbd843a11755b774156fdc6630c19b393fc8
Bex-G <bexgiacinto@gmail.com>
2026-02-04 19:39:40 -0800

    Rewrote program using grid instead of flexbox, fixed newSketchpad calculation bugs, updated UI.

--- script.js
+++ script.js
@@ -1,121 +1,34 @@
-const container = document.querySelector('#container');
+const root = document.documentElement;
 
-function draw(box) {
-    box.addEventListener('mouseover', (e) => {
-        e.target.style.background = 'black';
-    })
-};
+root.style.setProperty("--number", "16");
 
-//Creates default 16x16 sketchpad that draws in black.
+const screen = document.getElementById("screen");
 
-for (i = 0; i < 16 * 16; i++) {
-    const box = document.createElement('box')
-    const boxContent = document.createTextNode('\u00A0')
-    
-    box.style.height = '6.25%';
-    box.style.width = '6.25%';
-    
-    box.appendChild(boxContent);
-    container.appendChild(box);
+document.addEventListener("DOMContentLoaded", function () {
+  generatePixels(16);
+});
 
-    draw(box);
-};
-
-function clearSketchpad() {
-    container.innerHTML = '';
-};
-
-function formatAsPercentage(n) {
-    return new Intl.NumberFormat('defult', {
-        style: 'percent',
-        minimumFractionDigits: 1,
-        maximumFractionDigits: 2
-    }).format(n / 100);
+function newSketchpad() {
+  let input = prompt(
+    "Choose a number between 16-100 to generate a new sketchpad grid.",
+    16,
+  );
+  if (isNaN(input) || input < 16 || input > 100) {
+    alert("Whoops! Invalid entry. Please choose a number between 16-100.");
+    input = 16;
+  }
+  screen.innerHTML = "";
+  generatePixels(input);
 }
 
-function findPixelPercentage(n) {
-    let pixelsPerRow = 100 / n;
-    return formatAsPercentage(pixelsPerRow);
+function generatePixels(input) {
+  root.style.setProperty("--number", input);
+  for (i = 0; i < input * input; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+    screen.appendChild(box);
+  }
 }
-
-function newSketchpad(n) {   
-
-    for (i = 0; i < n*n; i++) {
-        const box = document.createElement('box')
-        const boxContent = document.createTextNode('\u00A0')
-        
-        box.style.height = findPixelPercentage(n)
-        box.style.width = findPixelPercentage(n)
-
-        box.appendChild(boxContent);
-        container.appendChild(box);
-
-        draw(box);
-    }
-};
-
-const newSketchpadButton = document.querySelector('#newSketchpadButton');
-
-newSketchpadButton.addEventListener('click', () => {
-
-    clearSketchpad();
-
-    let n = parseInt(prompt("How many pixels would you like on either side?"));
-    
-    if (n > 100 || n < 16) {
-        let n = parseInt(prompt("Sorry, please choose a number between 16 and 100."));
-        newSketchpad(n);
-    }
-    newSketchpad(n);
-    }
-);
-
-//The following functions enable the user to draw on a new sketchpad in randomly generated colors.
-
-function getRandomColor() {
-
-    let maxVal = 0xFFFFFF;
-    let randomNumber = Math.random() * maxVal;
-    randomNumber = Math.floor(randomNumber);
-    randomNumber = randomNumber.toString(16);
-    let randomColor = randomNumber.padStart(6, 0);
-    return `#${randomColor.toUpperCase()}`;
-};
-
-function drawColor(box) {
-    box.addEventListener('mouseover', (e) => {
-        e.target.style.background = getRandomColor();
-    })
-};
-
-function colorSketchpad(n) {   
-
-    for (i = 0; i < n*n; i++) {
-        const box = document.createElement('box')
-        const boxContent = document.createTextNode('\u00A0')
-        
-        box.style.height = findPixelPercentage(n)
-        box.style.width = findPixelPercentage(n)
-
-        box.appendChild(boxContent);
-        container.appendChild(box);
-
-        drawColor(box);
-    }
-};
-
-const colorButton = document.querySelector('#colorButton');
-
-colorButton.addEventListener('click', () => {
-
-    clearSketchpad();
-
-    let n = parseInt(prompt("How many pixels would you like on either side?"));
-    
-    if (n > 100 || n < 16) {
-        let n = parseInt(prompt("Sorry, please choose a number between 16 and 100."));
-        colorSketchpad(n);
-    }
-    colorSketchpad(n);
-    }
-);
